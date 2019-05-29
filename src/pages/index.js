8ea258baeab6acfe03e643e9a71979a70a60b56a
Patrick Lewis <patricklewis@patricks-mbp.home>
2019-05-29 15:14:22 -0400

    Add hero background image and style hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Ensuring care for the ones you love.</h1>
-    <p>Hello Jed gives a complete picture of a patient's health to the entire care team including doctors, at-home nurses, and family members.</p>
-    <Link to="/page-2/">Learn More</Link>
+    <div className="hero">
+      <div className="herogroup">
+        <h1>Ensuring care for the ones you love.</h1>
+        <p>Hello Jed gives a complete picture of a patient's health to the entire care team including doctors, at-home nurses, and family members.</p>
+        <Link to="/page-2/">Learn More</Link>
+      </div>
+    </div>
   </div>
 )
 
