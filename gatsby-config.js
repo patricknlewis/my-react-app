3c832a5d7f0a33b06ca6d3680e198714c20591f9
Patrick Lewis <patricklewis@patricks-mbp.home>
2019-05-29 14:20:47 -0400

    Update watch button text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Hello Jed',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Ensuring care for the ones you love.</h1>
+    <p>Hello Jed gives a complete picture of a patient's health to the entire care team including doctors, at-home nurses, and family members.</p>
+    <Link to="/page-2/">Learn More</Link>
   </div>
 )
 
