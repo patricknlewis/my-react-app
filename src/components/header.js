8460f4d85e763e8773decc8f2216964675fc5fd7
Patrick Lewis <patricklewis@patricks-mbp.home>
2019-05-30 14:00:42 -0400

    Reinstalled npm and added logo image with card component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from '../images/img_logo.svg';
 import './header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="header">
     <div className="headergroup">
-      <Link to="/">Logo</Link>
+      <Link to="/"><img src={logo} className="logo" /></Link>
       <Link to="/families">For families</Link>
       <Link to="/doctors">For doctors</Link>
       <Link to="/about">About us</Link>
