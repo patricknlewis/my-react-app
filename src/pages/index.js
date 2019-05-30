03d8fc2beb54002a2418ac283eb1468c1114cef1
Patrick Lewis <patricklewis@patricks-mbp.home>
2019-05-30 01:26:42 -0400

    Add animations to hero and header

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ const IndexPage = () => (
       <div className="herogroup">
         <h1>Get the complete picture of your loved one’s care.</h1>
         <p>Hello Jed gives a patient's care team - doctors, nurses, and family members - a place to communicate, stay up to date, and work together.</p>
-        <Link to="/page-2/">Text me the link</Link>
       </div>
     </div>
   </div>
